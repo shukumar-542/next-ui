f6bd4514e31ac453ba6f945cae3f81fd9ea3b56e
shukumar-542 <shukumar542@gmail.com>
2025-11-05 17:27:07 +0600

    Implement theme provider

--- components/ui/spotlight.tsx
+++ components/ui/spotlight.tsx
@@ -6,11 +6,11 @@ export type SpotlightProps = {
   fill?: string;
 };
 
-export function Spotlight({ className, fill }: SpotlightProps) {
+export const Spotlight: React.FC<SpotlightProps> = ({ className, fill }) => {
   return (
     <svg
       className={cn(
-        "pointer-events-none absolute z-1 h-[169%] w-[138%] lg:w-[84%] opacity-80",
+        "animate-spotlight pointer-events-none absolute z-[1] h-[169%] w-[138%] lg:w-[84%] opacity-20",
         className
       )}
       xmlns="http://www.w3.org/2000/svg"
@@ -24,10 +24,33 @@ export function Spotlight({ className, fill }: SpotlightProps) {
           rx="1924.71"
           ry="273.501"
           transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
-          fill={fill || "red"}
+          fill={fill || "white"}
           fillOpacity="0.21"
         />
       </g>
+      <defs>
+        <filter
+          id="filter"
+          x="0.860352"
+          y="0.838989"
+          width="3785.16"
+          height="2840.26"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="151"
+            result="effect1_foregroundBlur_1065_8"
+          />
+        </filter>
+      </defs>
     </svg>
   );
-}
+};
